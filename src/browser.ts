f251ab4389ca8cab7449e9e00485f5808edc37d1
Andrew Mackrodt <andrewmackrodt@gmail.com>
2025-02-08 19:21:38 +0000

    chore: enable headless gpu acceleration

--- src/browser.ts
+++ src/browser.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import os from 'node:os';
 import type {Browser, BrowserContext, LaunchOptions, Page} from 'puppeteer';
 import Puppeteer from 'puppeteer';
 import {enableBlockerInPage} from './adblocker';
@@ -14,17 +16,15 @@ export async function launchBrowser(options?: LaunchOptions): Promise<Browser> {
     `--window-size=${config.page.width},${config.page.height}`,
   ];
 
-  // Skip Chromium Linux Sandbox
-  // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
-  if (config.browser.isTrusted) {
+  // Skip Chromium Linux Sandbox (links possibly outdated)
+  // - https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
+  // - https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#tips
+  // - https://stackoverflow.com/questions/48230901/docker-alpine-with-node-js-and-chromium-headless-puppeter-failed-to-launch-c
+  if (config.browser.isTrusted || config.docker) {
     args.push('--no-sandbox');
   }
 
-  // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#tips
-  // https://stackoverflow.com/questions/48230901/docker-alpine-with-node-js-and-chromium-headless-puppeter-failed-to-launch-c
   if (config.docker) {
-    args.push('--disable-gpu');
-    args.push('--no-sandbox');
     config.browser.open = false;
   }
 
@@ -32,8 +32,52 @@ export async function launchBrowser(options?: LaunchOptions): Promise<Browser> {
   if (config.browser.isHeadless || config.docker) {
     args.push(
       '--headless=new',
+      '--ozone-platform-hint=auto',
       `--ozone-override-screen-size=${config.page.width},${config.page.height}`
     );
+
+    // TODO add a feature flag to make GPU enablement opt-in
+    let gpuAcceleration = false;
+
+    // using a GPU may help reduce system load and potentially reduce anti-bot
+    // detection, although is unconfirmed; it does however help pass
+    // https://deviceandbrowserinfo.com/are_you_a_bot
+    switch (os.platform()) {
+      // is metal renderer always available on mac? only tested with m1
+      case 'darwin':
+        // mac uses `--angle=` versus `--use-angle=` on linux
+        args.push('--angle=metal');
+        gpuAcceleration = true;
+        break;
+
+      case 'linux':
+        // only enable if /dev/dri/renderD128 exists; this is a primitive check
+        if (fs.existsSync('/dev/dri/renderD128')) {
+          // enable only one of GL or Vulkan, Vulkan takes precedence if both set
+          args.push(
+            // enable GL (default on Linux)
+            // only passes are_you_a_bot when `--use-angle=gl` but not available for nvidia gpu)
+            //'--use-angle=gl-egl'
+
+            // enable Vulkan (non-default on Linux)
+            // https://github.com/jasonmayes/headless-chrome-nvidia-t4-gpu-support/blob/main/README.md#4-run-nodejs-code-to-use-and-automate-headless-chrome
+            '--disable-vulkan-surface',
+            '--enable-features=Vulkan',
+            '--enable-unsafe-webgpu',
+            '--use-angle=vulkan',
+
+            // enable Accelerated Video Encoder (non-default on Linux)
+            // https://source.chromium.org/chromium/chromium/src/+/main:media/base/media_switches.cc;l=694?q=AcceleratedVideoDecodeLinuxGL&ss=chromium
+            '--enable-features=AcceleratedVideoEncoder'
+          );
+          gpuAcceleration = true;
+        }
+        break;
+    }
+
+    if (!gpuAcceleration) {
+      args.push('--disable-gpu');
+    }
   }
 
   // Add the address of the proxy server if defined
